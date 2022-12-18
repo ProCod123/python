bffd065b7c3ceb16cda076dbf8a34fc6d4042025
AleksandrJuk <aleksandrjuk12@gmail.com>
2022-12-18 14:33:05 +0300

    Семинар 2

---
+++ seminar2/task1.py
@@ -0,0 +1,19 @@
+#1) Напишите программу, которая принимает на вход число N и 
+#выдает набор произведений чисел от 1 до N.
+#Пример:
+#пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+number = int(input('Введите число: '))
+list = []
+
+rezl =[]
+for i in range(1, number + 1):
+    fac = 1
+    res = ''
+    for j in range(1, i + 1):
+        fac = fac * j
+        res += '*' + str(j) 
+    list.append(fac)
+    rezl.append(res[1:])
+    
+print(list, tuple(rezl))
