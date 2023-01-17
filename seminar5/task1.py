1c7854e04023184c9239f59b4b36a229138d503f
Aleksander <aleksandrjuk12@gmail.com>
2023-01-17 17:54:13 +0300

    Семинар 5

--- seminar5/task1.py
+++ seminar5/task1.py
@@ -14,9 +14,13 @@ def next_move(candy, move):
         human_move = int(input('Введите количество конфет: '))
         amount -= human_move
     elif move == 'computer':
-        computer_move = random.randint(1,28)
-        amount -= computer_move
-        print(f'Компьютер взял {computer_move} конфет')
+        if amount > 28:
+            computer_move = random.randint(1,28)
+            amount -= computer_move
+            print(f'Компьютер взял {computer_move} конфет')
+        elif amount <= 28:
+            print(f'Компьютер взял {amount} конфет')
+            amount = 0
     return amount 
 
 
